a7b1c91de9c133034ca350b1be381008148d8be7
Saul <saul.garcia@kipup.mx>
2021-09-03 16:24:00 -0600

    privvacy policy

---
+++ privacy-policy.php
@@ -0,0 +1,113 @@
+
+    <div id="adp">
+        <section>
+            <h1>Privacy statement</h1>
+            <p>This Privacy Statement explains our practices regarding the collection, use, transfer and transmission of certain information, including, your personal information by the King Logistics family of companies ("King").</p>
+        </section>
+
+        <section>
+            <h3>Contact</h3>
+            <p>If you have any questions regarding your account, please contact our help center at +1 (800) 569-5174 or by email at info@kinglogisticoil.com, where you will also find information on how to contact our customer service for assistance. If you have any questions regarding this Privacy Statement, or the use of your personal information, cookies and other similar technologies, please write to us at info@kinglogisticoil.com.</p>
+            <p>In all other cases, King provides the service and is responsible for the personal data collected (where applicable).</p>
+            <p class="lot">Please note that if you contact us for assistance, both for your security and ours, we may ask you to authenticate your identity before making the request.</p>
+        </section>
+
+        <section>
+            <h3>Information gathering</h3>
+            <p>We receive and store information about you, including:</p>
+            <ul>
+                <li><b>Information you provide to us:</b> We collect information that you provide to us, including:
+                    <ul>
+                        <li>Names and surnames, email, address, postal code, payment method and telephone number. This information is collected in a number of ways, including when you enter it on our service, interact with our customer service, or participate in surveys or marketing promotionsç</li>
+                        <li class="lot">information collected when you choose to provide reviews or ratings, when you set "My Account" and "My Company" preferences, your general preferences, or provide information to us through our service or other means.</li>
+                    </ul>
+                </li>
+                <li><b>The information we collect automatically:</b> We collect information related to you, the use of our service and your interactions with us and our advertising, also that information related to your computer or any device that you use to access our service (such as the systems of video games, smart TV, mobile devices and set-top boxes). This information includes:
+                    <ul>
+                        <li>Your activity on the King service.</li>
+                        <li>Details about your interactions with customer service, such as the date, time, reason for the inquiry, transcripts of any chat conversations, and if you call us, your phone number.</li>
+                        <li>ID de dispositivos o identificadores únicos, características de software y disposito    Device IDs or unique identifiers, software and device characteristics (such as type and configuration), connection information, page display statistics, referrer URLs, advertising data, IP addresses, geolocation, and standard web log information.</li>
+                        <li class="lot">o   Information collected through the use of cookies, web beacons, and other technologies.</li>
+                    </ul>
+                </li>
+                <li class="lot"><b>Information from other sources: </b> We can supplement the information described above with information obtained from other sources, including data providers obtained both online and offline. This supplemental information may include demographic data, data based on Internet browsing behaviors and interests.</li>
+            </ul>
+        </section>
+
+        <section>
+            <h3>Use of information</h3>
+            <p>We use the information collected to provide, analyze, administer, improve and personalize our services and marketing initiatives, registration, ordering and payments processed; and to communicate with you about this and other topics. For example, we use the information we collect to:</p>
+            <ul>
+                <li>Determine the type of business and its activities, offer you options appropriate to your line of business, recommend personalized options for that type of work, determine suppliers near your area and to help us respond more quickly and efficiently to inquiries and requests.</li>
+                <li>Prevent, detect, and investigate potentially prohibited or illegal activities, such as fraud, and enforce our terms of use (for example, determine whether to offer you a free trial period).</li>
+                <li>Analyze and understand our audience; improve service (including user interface experience), delivery optimization, content selection.</li>
+                <li class="lot">Communicate with you in relation to our service (for example, by email, notifications and text messages), to be able to send you news about King, details about new features and content available on King, as well as special offers, promotional announcements and surveys for consumers, and to assist you with operational requests, such as resetting your password.</li>
+            </ul>
+        </section>
+
+        <section>
+            <a name=""></a>
+            <h3>Information transfer</h3>
+            <p>We transfer your information for certain purposes and to third parties, as follows:</p>
+            <ul>
+                <li><b>The King Family of Companies:</b> We share your information with the King Family of Companies as necessary to: process and store data; provide access to our services; provide customer service; make decisions about service improvements and content development; and as described in the Use of Information section of this Privacy Statement.</li>
+                <li><b>Service Providers:</b> We use other companies, agents or contractors (“Service Providers”) to perform services on our behalf or to assist us in providing the service. For example, we engage service providers to offer marketing, communications, infrastructure and IT services, which personalize and improve our service, process credit card and other payment transactions, provide customer service, collect debts, analyze and improve data, including information regarding our users interaction with our website, and process our consumer surveys. In the course of providing such services, these companies may have access to your personal or other information. We do not authorize them to use or transfer your personal information, unless it is related to the services they provide.</li>
+                <li><b>Promotions:</b> We may offer promotions or programs that require the exchange of information with third parties as a condition of participation. By conducting these types of promotions, we may share your name and other information related to the performance of the incentive. Please note that third parties are responsible for their own privacy practices. </li>
+                <li><b>King’s Protection and others:</b> King and its service providers may transfer personal information when we consider or reasonably consider that such transfer is necessary to (a) comply with any applicable legislation, regulation, legal process or governmental request; (b) enforce applicable terms of use, including investigation of possible violations; (c) detect, prevent or address illegal or suspected illegal activities (including fraudulent payment), security or technical problems; or (d) protect from harm to the rights, property, or King’s safety, its users, or the public, as required or permitted by law.</li>
+                <li><b>Business Transfers: </b> In connection with a reorganization, restructuring, merger or sale, or any other transfer of assets, we will transfer information, including personal information, provided the recipient agrees to respect your personal information in accordance with our Privacy Statement. </li>
+            </ul>
+            <p>When we share personal information with countries outside the USA Economic Area and other regions that have comprehensive data protection laws, we will ensure that such information is transferred in accordance with this Privacy Statement and the corresponding laws on data protection.</p>
+            <p>You can also choose to transfer your information in the following ways:</p>
+            <ul>
+                <li>By using the King service, you can post reviews or other public information and third parties can use that information.</li>
+                <li>Certain areas of our service may include a tool that allows you to share information by email, SMS text message, and social or other applications through clients and applications on your smart device</li>
+                <li class="lot">Social plugins or add-ons (including those offered by Facebook, Twitter, Pinterest and Google) allow you to share information through those platforms.</li>
+            </ul>
+            <p class="lot">The same social networks handle these plugins and these applications, and are subject to their terms of use and privacy policies.</p>
+        </section>
+
+        <section>
+            <h3>Access to "My account" and profiles</h3>
+            <p>If you share or allow others to access your account, they will be able to view your information, including, in some cases, personal information such as your purchase, sales, customer, billing history and all related areas and account information (including your email address or other information in "My Account").</p>
+            <p>The types of users allow the different members of your Work Team to have their own personalized King experience, according to the area in which your staff works. Take into consideration that the profiles will be freely available to anyone who uses your King account, anyone who has access to your King administrator account can navigate to, use, edit or delete any profile. This must be explained to the other people who have access to your account, if you do not want them to use or update your profile, make sure they are aware of this situation.</p>
+        </section>
+
+        <section>
+            <a name="Security"></a>
+            <h3>Security</h3>
+            <p class="lot">We believe we have reasonable administrative, technical, physical, and managerial measures to protect your personal information against loss, theft, and unauthorized access, use, and modification. Unfortunately, no security system is 100% secure. Based on this, we cannot guarantee the security of your information.</p>
+        </section>
+
+        <section>
+            <a name="other"></a>
+            <h3>Other sites, platforms and applications</h3>
+            <p class="lot">The King service can be received through platforms or contain links to sites operated by third parties that may have policies regarding the handling of information different from ours. For example, you can access the King service through platforms such as video game systems, smart TVs, mobile devices, set-top boxes, and a variety of devices with an Internet connection. These websites and platforms have their own privacy statements, data policies, notices, and terms of use, which we recommend that you read carefully. Also, you will likely find third-party applications that interact with the King service.</p>
+        </section>
+
+        <section>
+            <a name="children"></a>
+            <h3>Kids</h3>
+            <p class="lot">You must be 18 years or older to subscribe to the King service. In some jurisdictions, the age of majority may be when you turn 18, in which case you must be old enough to subscribe. While minors under the age of 18 may use the service, they can only do so with the participation, supervision, and approval of a parent or guardian.</p>
+        </section>
+
+
+        <section>
+            <a name="changes"></a>
+            <h3>Changes to this Privacy Statement</h3>
+            <p class="lot">We will update this Privacy Statement from time to time in accordance with changes in legal, regulatory or operational requirements. We will notify you of such changes (including information on the implementation date) in accordance with the law. If you continue to use the King service after the updates take effect, it will be construed as acceptance of the changes. If you do not accept any of the updates to this Privacy Statement, you can cancel and stop using the King service. </p>
+        </section>
+
+        <section>
+            <a name="cookies"></a>
+            <h3>Cookies and Internet Advertising</h3>
+            <p>We and our Service Providers use cookies and other technologies (such as web beacons) for different reasons. For example, we use cookies and other technologies to facilitate access to our services and remember you when you re-enter, provide and analyze our services, learn more about our users and their likely interests, and present and personalize marketing or advertising. We want you to be informed about our use of these technologies, so this notice explains the types of technology we use, how they behave, and what your options for use are.</p>
+            <ul>
+                <li><b>What are cookies?</b>
+                    <p>Cookies are small data files that are usually stored on your device when you browse and use websites and other online services. They are used extensively to make websites work, or to be more efficient, as well as to provide reporting information and help with service or advertising personalization.</p>
+                    <p class="lot">Cookies are not the only type of technology that allows operation; We also use other similar technologies (session files, "local storage", "session storage", among others).</p>
+                </li>
+            </ul>
+        </section>
+
+        <section id="last_updated"><b>Date created:</b> September 1, 2021</section>
+    </div>
